68fb1469b12db77572f81423b959bee317fd049c
Ivan <45919772+ivanMuzyka144@users.noreply.github.com>
2020-02-23 17:39:21 +0100

    Add basic shoot logic

---
+++ Assets/Scripts/NewScripts/NewBullet.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NewBullet : MonoBehaviour
+{
+    private bool canMove;
+    private Vector3 destinationVector;
+
+    private void Update()
+    {
+        if (canMove)
+        {
+            transform.position = Vector3.MoveTowards(transform.position, destinationVector, 0.01f);
+        }
+    }
+
+    public void SetDistinationVector(Vector3 vector)
+    {
+        destinationVector = vector;
+        canMove = true;
+    }
+}
---
+++ Assets/Scripts/NewScripts/NewWeapon.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NewWeapon : MonoBehaviour
+{
+    public List<GameObject> spawnPointList = new List<GameObject>();
+    public GameObject bullet;
+    public Camera myCamera;
+
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            Fire();
+            Debug.Log("Pressed primary button.");
+        }
+    }
+
+    public void Fire()
+    {
+        SetBulletsToSpawnPoints();
+    }
+    public void SetBulletsToSpawnPoints()
+    {
+        foreach(GameObject spawnPoint in spawnPointList)
+        {
+            GameObject newBullet = GameObject.Instantiate(bullet);
+            newBullet.transform.position = spawnPoint.transform.position;
+            Debug.Log(newBullet.transform.localScale);
+
+            Ray ray = Camera.main.ViewportPointToRay(new Vector3(0.5f, 0.5f, 0.0f));
+            RaycastHit hit;
+            ray.origin = spawnPoint.transform.position;
+            if (Physics.Raycast(ray, out hit, 10000))
+            {
+                Debug.Log(hit.point);
+                newBullet.GetComponent<NewBullet>().SetDistinationVector(hit.point);
+            }
+                //newBullet.GetComponent<NewBullet>().SetDistinationVector(laserEnd);
+            }
+    }
+}
